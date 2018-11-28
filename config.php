6cd94fdc55b62c75d1b600bfb36f17cc6d7e9412
Mrc0c0 <43548278+Mrc0c0@users.noreply.github.com>
2018-11-28 20:58:21 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh4TOr7qNabO1lJeyepiyioaik4K2k2C1eIVihKOvmIuKi5Wvup9ps7Wckq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "a826971210a025a6";
+$os="android";
+$sign="047cf2eb4874ea6f1e748d4fa5c8bf16673b5c69d99fea6b2744a68b6381f915";
